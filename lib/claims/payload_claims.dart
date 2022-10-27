756cbdfa1812ded5f533c57bb1d6c93035044aa6
Luciano Yamane <luciano.oliveira@klab.com.br>
2022-10-27 11:21:20 -0300

    Refactoring claims class to get jwtBuilder parameter

---
+++ lib/claims/payload_claims.dart
@@ -0,0 +1,35 @@
+import 'package:jsonwebtoken_decode/claims/claims.dart';
+import 'package:jsonwebtoken_decode/jsonwebtoken_decode.dart';
+
+class PayloadClaims extends Claims {
+  late Payload payload;
+
+  PayloadClaims(super.jwtBuilder) {
+    payload = jwtBuilder.payload;
+  }
+
+  claim(String value) => payload.claim(value);
+
+  get iss => claim("iss");
+
+  get exp => claim("exp");
+
+  get iat => claim("iat");
+
+  get jti => claim("jti");
+
+  get aud => claim("aud");
+
+  get sub => claim("sub");
+
+  get scope => claim("scope");
+
+  DateTime? get expirationDate => payload.claimDate('exp');
+
+  bool get isExpired {
+    final expirationDateValue = expirationDate;
+    return expirationDateValue != null
+        ? DateTime.now().isAfter(expirationDateValue)
+        : false;
+  }
+}
